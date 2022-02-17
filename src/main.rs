ae49121fd77e039e3b28ef10af67bc9dc2c063e2
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-18 09:46:29 +1100

    error catchers + more
    + Update routes to return status codes\n+ Added configuration

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@ extern crate rocket;
 extern crate base64;
 
 use base64::{decode as d64, encode as e64};
+use rocket::http::Status;
+use rocket::response::status;
+use rocket::{Config, Request};
 use std::str;
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("{} Is not a valid Route", req.uri())
+}
+
+#[catch(500)]
+fn backend_flipped(_req: &Request) -> String {
+    format!("{}", "Something went wrong in the Backend!")
+}
 
 #[get("/")]
 fn home() -> &'static str {
@@ -13,18 +26,43 @@ fn home() -> &'static str {
 }
 
 #[get("/encode?<text>")]
-fn encode(text: &str) -> String {
-    e64(text.to_string()).to_string()
+fn encode(text: &str) -> status::Custom<String> {
+    let string = text.to_string();
+
+    if string.is_empty() {
+        return status::Custom(Status::BadRequest, String::from("text parameter required"));
+    }
+
+    return status::Custom(Status::Ok, e64(text.to_string()).to_string());
 }
 
 #[get("/decode?<data>")]
-fn decode(data: &str) -> String {
-    str::from_utf8(&d64(data.to_string()).unwrap())
-        .unwrap_or(&String::from(""))
-        .to_string()
+fn decode(data: &str) -> status::Custom<String> {
+    if data.to_string().is_empty() {
+        return status::Custom(Status::BadRequest, "data parameter required".to_string());
+    }
+
+    let bytes = &d64(data.to_string()).unwrap_or(Vec::new());
+
+    if bytes.len() == 0 {
+        return status::Custom(Status::BadRequest, "invalid base64 data".to_string());
+    }
+
+    let decoded = str::from_utf8(bytes);
+
+    return status::Custom(Status::Ok, decoded.unwrap().to_string());
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![encode, decode, home])
+    let config = Config {
+        port: 8080,
+        keep_alive: 60,
+        ..Config::debug_default()
+    };
+
+    rocket::build()
+        .configure(config)
+        .register("/", catchers![not_found, backend_flipped])
+        .mount("/", routes![encode, decode, home])
 }
