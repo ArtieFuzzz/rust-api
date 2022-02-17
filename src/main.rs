6306da5e9a18e462979d08a981d0754a34b51530
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-17 18:50:48 +1100

    More Routes!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-// use uri_pct;
-
 #[macro_use]
 extern crate rocket;
+extern crate base64;
+
+use base64::{decode as d64, encode as e64};
+use std::str;
+
+#[get("/")]
+fn home() -> &'static str {
+    "Hello User!"
+}
 
 #[get("/encode?<text>")]
 fn encode(text: &str) -> String {
-    text.to_string()
+    e64(text.to_string()).to_string()
+}
+
+#[get("/decode?<data>")]
+fn decode(data: &str) -> String {
+    str::from_utf8(&d64(data.to_string()).unwrap())
+        .unwrap()
+        .to_string()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![encode])
+    rocket::build().mount("/", routes![encode, decode, home])
 }
