6f3423c1d1425c17aa5b7a747eb5ad59a1a36f0a
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-28 16:29:48 +1100

    HTML + Shield

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,24 @@ mod guards;
 mod routes;
 
 use fairings::powered_by;
+use rocket::response::content::RawHtml;
+use rocket::shield::{Referrer, Shield, XssFilter};
 use routes::{base64 as base_64, binary, token};
 
 #[get("/")]
-fn home() -> &'static str {
-    "Hello User!"
+fn home() -> RawHtml<&'static str> {
+    RawHtml(
+        "<h1>Hewwo! Welcome to the backend API</h1>
+    <p>Right now there is not documentation, why?, Cuz no >:3</p>",
+    )
 }
 
 #[launch]
 fn rocket() -> _ {
+    let armour = Shield::default()
+        .enable(XssFilter::Disable)
+        .enable(Referrer::NoReferrer);
+
     let config = Config {
         port: 8080,
         keep_alive: 60,
@@ -30,6 +39,7 @@ fn rocket() -> _ {
     rocket::build()
         .configure(config)
         .attach(powered_by::PoweredBy::default())
+        .attach(armour)
         // Register the Error catchers
         .register(
             "/",
